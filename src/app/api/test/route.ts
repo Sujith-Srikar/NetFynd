5ae82b57e59e98a1d765d9c7a7576329966de1e0
Sujith Srikar <kandregulasujith@gmail.com>
2025-02-11 14:04:47 +0530

    build fix

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import {  NextResponse } from "next/server";
 import { connectDB } from "@/lib/services/db";
 import Investor from "@/lib/models/Investor";
 
